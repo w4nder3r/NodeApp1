bdcee8949826ceba7e1dd40c3811ba4a00c2cbb4
Farooqui <faroabd@amazon.com>
2023-06-16 01:48:50 +0530

    Initial commit

--- app.js
+++ app.js
@@ -1,31 +1,35 @@
+const Redis = require('ioredis');
 const express = require('express');
-const multer = require('multer');
-const xlsx = require('xlsx');
-const redis = require('redis');
-
 const app = express();
-const upload = multer({ dest: 'uploads/' });
-const client = redis.createClient();
+const bodyParser = require('body-parser');
 
-app.post('/upload', upload.single('file'), (req, res) => {
-  const file = req.file;
-  if (!file) {
-    return res.status(400).json({ error: 'No file uploaded.' });
+// Configure Redis connection
+const redis = new Redis();
+
+// Parse JSON bodies
+app.use(bodyParser.json());
+
+// Handle POST request to store user data
+app.post('/storeUserData', async (req, res) => {
+  try {
+    const { name, age, phone, email, city } = req.body;
+
+    // Store user data in Redis
+    await redis.hmset('user', ['name', name, 'age', age, 'phone', phone, 'email', email, 'city', city]);
+
+    res.json({ message: 'User data stored in Redis successfully!' });
+  } catch (error) {
+    console.error('Error storing user data in Redis:', error);
+    res.status(500).json({ message: 'An error occurred while storing user data.' });
   }
-
-  const workbook = xlsx.readFile(file.path);
-  const sheetName = workbook.SheetNames[0];
-  const sheetData = xlsx.utils.sheet_to_json(workbook.Sheets[sheetName]);
-
-  sheetData.forEach((row) => {
-    const key = `data:${row.id}`;
-    const value = JSON.stringify(row);
-    client.set(key, value);
-  });
-
-  res.json({ message: 'Data uploaded to Redis successfully.' });
 });
 
+// Serve the frontend HTML file
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html');
+});
+
+// Start the server
 app.listen(3000, () => {
-  console.log('Server is running on port 3000.');
+  console.log('Server started on port 3000');
 });
